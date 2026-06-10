e30241fc708bc02be9f5578e74e80fc648d607a3
Githendra Perera <gpayagalage@gmail.com>
2026-06-10 16:13:47 +0200

    [+] ajout de la classe Roues

---
+++ classes/Roues.py
@@ -0,0 +1,19 @@
+import ServoController
+
+class Roues:
+    def __innit__(self, controller: ServoController):
+        CHANNEL = 0
+        
+        self.controller = controller
+        self.controller.add_servo(CHANNEL)
+        self.controller.set_angle(90)
+    
+    def turn(self, angle):
+        ANGLE_MIN = 0
+        ANGLE_MAX = 130
+        
+        if (angle >= ANGLE_MIN and angle <= ANGLE_MAX):
+            self.controller.set_angle(angle)
+            
+    def reset(self):
+        self.controller.set_angle(90)
