25c5ac2d60dc1c6e9375738a2ac38236dab9b49d
Stevekgs28 <stevekingsley2005@gmail.com>
2026-06-13 14:32:49 +0200

    Ajout d'une fonction bip()

--- classes/Buzzer.py
+++ classes/Buzzer.py
@@ -2,6 +2,9 @@
 from gpiozero import TonalBuzzer
 from time import sleep
 
+import Musics
+
+
 # Initialize a TonalBuzzer connected to GPIO18 (BCM)
 class Buzzer:
     def __init__(self): 
@@ -19,23 +22,19 @@ class Buzzer:
             sleep(float(duration))  # Delay for the duration of the note
             self.tb.stop()  # Stop playing after the tune is complete
 
-if __name__ == "__main__":
-    # Define a musical tune as a sequence of notes and durations.
-    SONG =	[
-    ["E5",0.3],["Eb5",0.3],
-    ["E5",0.3],["Eb5",0.3],["E5",0.3],["B4",0.3],["D5",0.3],["C5",0.3],
-    ["A4",0.6],[None,0.1],["C4",0.3],["E4",0.3],["A4",0.3],
-    ["B4",0.6],[None,0.1],["E4",0.3],["Ab4",0.3],["B4",0.3],
-    ["C5",0.6],[None,0.1],["E4",0.3],["E5",0.3],["Eb5",0.3],
-    ["E5",0.3],["Eb5",0.3],["E5",0.3],["B4",0.3],["D5",0.3],["C5",0.3],
-    ["A4",0.6],[None,0.1],["C4",0.3],["E4",0.3],["A4",0.3],
-    ["B4",0.6],[None,0.1],["E4",0.3],["C5",0.3],["B4",0.3],["A4",0.1]
-    ]
+    def bip(self) :
+        note = "D4"
+        duration = 0.5
+        self.tb.play(note)  # Play the note on the buzzer
+        sleep(float(duration))  # Delay for the duration of the note
+        self.tb.stop()  # Stop playing after the tune is complete
+        sleep(float(duration))
     
+if __name__ == "__main__":    
     try:
         buzzer = Buzzer()
-        buzzer.play(SONG)  # Execute the play function to start playing the tune.
-
+        buzzer.bip()
+        
     except KeyboardInterrupt:
         # Handle KeyboardInterrupt for graceful termination
         pass
