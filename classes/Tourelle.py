42c7c11303912a6df792188ee8f918805065c741
Stevekgs28 <stevekingsley2005@gmail.com>
2026-06-14 20:38:38 +0200

    Renommages et tests

--- classes/Tourelle.py
+++ classes/Tourelle.py
@@ -1,28 +1,53 @@
+import time
+
 import ServoController
+
+ANGLE_MAX = 180
+ANGLE_MIN = 0
+
 
 class Tourelle:
     def __init__(self, controller : ServoController):
-        self.CHANNEL_PAN = 1 # horizontal
-        self.CHANNEL_TILT = 2 # vertical
+        self.CHANNEL_X_AXIS = 1 # horizontal
+        self.CHANNEL_Y_AXIS = 2 # vertical
         
         self.controller = controller
-        self.controller.add_servo(self.CHANNEL_PAN)
-        self.controleler.add_servo(self.CHANNEL_TILT)
+        self.controller.add_servo(self.CHANNEL_X_AXIS)
+        self.controller.add_servo(self.CHANNEL_Y_AXIS)
         
-    def pan(self, angle):
-        ANGLE_MAX = 180
-        ANGLE_MIN = 0
-
+    def turn_x_axis(self, angle):
         if (angle >= ANGLE_MIN and angle <= ANGLE_MAX):
-            self.controller.set_angle(self.CHANNEL_PAN, angle)
+            self.controller.set_angle(self.CHANNEL_X_AXIS, angle)
 
-    def tilt(self, angle):
-        ANGLE_MAX = 180
-        ANGLE_MIN = 0
-
+    def turn_y_axis(self, angle):
         if (angle >= ANGLE_MIN and angle <= ANGLE_MAX):
-            self.controller.set_angle(self.CHANNEL_TILT, angle)
+            self.controller.set_angle(self.CHANNEL_Y_AXIS, angle)
+
+
+    def print_angle(self) :
+        print(f"Angle X : ")
+        print(f"Angle Y : ")
 
     def reset(self):
-        self.controller.set_angle(self.CHANNEL_PAN, 97)
-        self.controller.set_angle(self.CHANNEL_TILT, 96)
+        self.turn_x_axis(90)
+        self.turn_y_axis(90)
+
+
+if __name__ == "__main__" :
+    servo = ServoController.ServoController()
+    tourelle = Tourelle(servo)
+
+    try :
+        while True :
+            x_angle = int(input("Entrez l'angle sur l'axe X : "))
+            y_angle = int(input("Entrez l'angle sur l'axe Y : "))
+
+            tourelle.turn_x_axis(x_angle)
+            tourelle.turn_y_axis(y_angle)
+            # tourelle.print_angle()
+            time.sleep(2)
+            tourelle.reset()
+            
+    except KeyboardInterrupt :
+        print("Fin du programme.")
+        tourelle.reset()
