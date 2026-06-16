e2f15a8b1bb4aa89d223985c38b3c1f11bf343d4
Stevekgs28 <stevekingsley2005@gmail.com>
2026-06-16 19:34:13 +0200

    tb devient tonalbuzzer pour plus de clarté; ajout d'un message pour interruption au clavier

--- classes/Buzzer.py
+++ classes/Buzzer.py
@@ -9,7 +9,7 @@ import Musics
 class Buzzer:
     def __init__(self): 
         self.GPIOpin = 18
-        self.tb = TonalBuzzer(self.GPIOpin)
+        self.tonalbuzzer = TonalBuzzer(self.GPIOpin)
 
     def play(self, tune):
         """
@@ -18,16 +18,16 @@ class Buzzer:
         where each tuple represents a note and its duration.
         """
         for note, duration in tune:
-            self.tb.play(note)  # Play the note on the buzzer
+            self.tonalbuzzer.play(note)  # Play the note on the buzzer
             sleep(float(duration))  # Delay for the duration of the note
-            self.tb.stop()  # Stop playing after the tune is complete
+            self.tonalbuzzer.stop()  # Stop playing after the tune is complete
 
     def bip(self) :
         note = "D4"
         duration = 0.5
-        self.tb.play(note)  # Play the note on the buzzer
+        self.tonalbuzzer.play(note)  # Play the note on the buzzer
         sleep(float(duration))  # Delay for the duration of the note
-        self.tb.stop()  # Stop playing after the tune is complete
+        self.tonalbuzzer.stop()  # Stop playing after the tune is complete
         sleep(float(duration))
     
 if __name__ == "__main__":    
@@ -37,4 +37,4 @@ if __name__ == "__main__":
         
     except KeyboardInterrupt:
         # Handle KeyboardInterrupt for graceful termination
-        pass
+        print("Fin du programme via le clavier.")
