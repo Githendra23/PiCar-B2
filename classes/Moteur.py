bf619b5697c2f039089e807121c359fd229987ca
Stevekgs28 <84630495+Stevekgs28@users.noreply.github.com>
2026-06-16 11:38:31 +0200

    Merge artificiel de la branche moteur
    Il y a des fichiers qui disparaissaient, j'ai donc copié/collé le fichier à la main.

--- classes/Moteur.py
+++ classes/Moteur.py
@@ -12,21 +12,8 @@ import sys
 VMIN = 0
 VMAX = 100
 
-MOTOR_M1_IN1 =  15      #Define the positive pole of M1
-MOTOR_M1_IN2 =  14      #Define the negative pole of M1
-
-pwn_A = 0
-pwm_B = 0
-
 def map(x,in_min,in_max,out_min,out_max):
   return (x - in_min)/(in_max - in_min) *(out_max - out_min) +out_min
-
-#def setup():
-i2c = busio.I2C(SCL, SDA)
-# Create a simple PCA9685 class instance.
-#  pwm_motor.channels[7].duty_cycle = 0xFFFF
-pwm_motor = PCA9685(i2c, address=0x5f) #default 0x40
-pwm_motor.frequency = 50
 
 # Pour bien garder la vitesse entre 0 et 100
 def check_speed(vitesse) :
@@ -38,42 +25,67 @@ def check_speed(vitesse) :
 
 class Moteur:
     def __init__(self):
-        self.moteur = motor.DCMotor(pwm_motor.channels[MOTOR_M1_IN1],pwm_motor.channels[MOTOR_M1_IN2] )
+        MOTOR_M1_IN1 =  15      #Define the positive pole of M1
+        MOTOR_M1_IN2 =  14      #Define the negative pole of M1
+        pwn_A = 0
+        pwm_B = 0
+
+        i2c = busio.I2C(SCL, SDA)
+        # Create a simple PCA9685 class instance.
+        #  pwm_motor.channels[7].duty_cycle = 0xFFFF
+        self.pwm_motor = PCA9685(i2c, address=0x5f) #default 0x40
+        self.pwm_motor.frequency = 50
+
+        self.moteur = motor.DCMotor(self.pwm_motor.channels[MOTOR_M1_IN1],self.pwm_motor.channels[MOTOR_M1_IN2])
         self.moteur.decay_mode = (motor.SLOW_DECAY)
+
+        self.current_speed = 0
     
     # Pour avancer
-    def avancer(self, vitesse):
-        vitesse = check_speed(vitesse)
-        print(f"Le moteur avance ! Vitesse : {vitesse}")
-        self.moteur.throttle = map(vitesse, 0, 100, 0, 1.0)
+    def drive(self, speed):
+        self.current_speed = speed
+        speed = check_speed(speed)
+        print(f"Le moteur avance ! Vitesse : {speed}")
+        # self.moteur.throttle = map(speed, 0, 100, 0, 1.0)
+        self.progress_start(speed,1)
 
     # Pour reculer
-    def reculer(self, vitesse):
-        vitesse = check_speed(vitesse)
-        print(f"Le moteur recule ! Vitesse : {vitesse}")
-        self.moteur.throttle = -map(vitesse, 0, 100, 0, 1.0)
+    def reverse(self, speed):
+        self.current_speed = speed
+        speed = check_speed(speed)
+        print(f"Le moteur recule ! Vitesse : {speed}")
+        # self.moteur.throttle = -map(speed, 0, 100, 0, 1.0)
+        self.progress_start(speed,-1)
 
-    # Pour un démarrage progressif afin de ne pas abîmer le différentiel
+    # Pour un démarrage progressif afin de ne pas abîmer la transmission
     def progress_start(self, speed, direction) :
+        acceleration_delay = 0.05
         speed = check_speed(speed)
         for i in range(speed) :
             if(direction < 0) :
               direction = -1
             else :
               direction = 1
-            unMoteur.moteur.throttle = direction*map(i,0,100,0,1.0)
-            time.sleep(0.05)
+            self.moteur.throttle = direction*map(i,0,100,0,1.0)
+            time.sleep(acceleration_delay)
   
     # Arrêter le moteur
     def stop(self):
         print("Le moteur est à l'arrêt !")
         self.moteur.throttle = 0
-    
+
+    # Contrôle de traction, si les roues motrices patinent
+    def TC(self, speed) :
+        self.stop()
+        time.sleep(1/20)
+        self.drive(speed)
+        time.sleep(1/20)
+
     # Déconnecter le moteur
     def destroy(self):
-        print("Destruction du moteur.")
+        # print("Destruction du moteur.")
         self.stop()
-        pwm_motor.deinit()
+        self.pwm_motor.deinit()
 
 # Création d'une instance du moteur
 unMoteur = Moteur()
@@ -84,12 +96,17 @@ if __name__ == '__main__':
         gear = int(sys.argv[1])
         if(gear == 0) : # Pour arrêter le moteur
             unMoteur.stop()
+
         elif(gear == 1) : # Pour avancer
-            speed = 30
-            unMoteur.progress_start(speed,1)
+            speed = 100
+            unMoteur.drive(speed)
+            time.sleep(2)
+
         elif(gear == 2) : # Pour reculer
             speed = 30
-            unMoteur.progress_start(speed,-1)
+            unMoteur.reverse(speed)
+            time.sleep(2)
+            
         else : # Si l'utilisateur ne saisit pas une bonne entrée
             print("0 pour NEUTRE")
             print("1 pour AVANCER")
