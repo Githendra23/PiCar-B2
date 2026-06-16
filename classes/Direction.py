d744c5acced04b2951efe93f03d75724a0f02169
Stevekgs28 <84630495+Stevekgs28@users.noreply.github.com>
2026-06-16 11:47:40 +0200

    Renommage : Roues devient Direction
    Plus clair, pas de confusion avec les roues arrières motrices.

---
+++ classes/Direction.py
@@ -0,0 +1,43 @@
+import ServoController
+
+import time
+
+class Direction:
+    def __init__(self):
+        self.servo = ServoController.ServoController()
+        self.CHANNEL = 0
+
+        self.ANGLE_MIN = 0
+        self.ANGLE_MAX = 140
+        self.ANGLE_CENTER = 90
+
+        self.servo.add_servo(self.CHANNEL)
+        self.reset()
+
+    def turn(self, angle):
+        if (angle >= self.ANGLE_MIN and angle <= self.ANGLE_MAX):
+            self.servo.set_angle(self.CHANNEL, angle)        
+
+    def getAngleMin(self):
+        return self.ANGLE_MIN
+
+    def getAngleMax(self):
+        return self.ANGLE_MAX
+
+    def getAngleCenter(self):
+        return self.ANGLE_CENTER
+
+    def reset(self):
+        self.servo.set_angle(self.CHANNEL, self.ANGLE_CENTER)
+
+
+if __name__ == "__main__" :
+    direction = Direction()
+    try :
+        while True :
+            angle = int(input("Entrez un angle : "))
+            direction.turn(angle)
+            time.sleep(0.5)
+    except KeyboardInterrupt :
+        print("Fin du programme.")
+        direction.reset()
