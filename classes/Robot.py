c11d5a4bf149dc0dab66322d1b4a5f2212595c0b
Stevekgs28 <stevekingsley2005@gmail.com>
2026-06-18 10:07:58 +0200

    Création de la classe Robot; ajout de quelques méthodes sans les définitions

---
+++ classes/Robot.py
@@ -0,0 +1,30 @@
+import time
+
+import Buzzer
+import Direction
+import Moteur
+import Tourelle
+
+
+class Robot :
+    def __init__(self):
+        pass
+
+    def drive(self) :
+        pass
+
+    def reverse(self) :
+        pass
+
+    def turnTourelleXAxis(self, percentage) :
+        pass
+    
+    def turnTourelleYAxis(self, percentage) :
+        pass
+
+
+if __name__ == "__main__" :
+    try :
+        pass
+    except KeyboardInterrupt :
+        pass
