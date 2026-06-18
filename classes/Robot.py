9c30db596ddd31a04e32b5a0c7b0e691e31290a6
Stevekgs28 <stevekingsley2005@gmail.com>
2026-06-18 10:11:51 +0200

    Définition des premières méthodes

--- classes/Robot.py
+++ classes/Robot.py
@@ -8,20 +8,26 @@ import Tourelle
 
 class Robot :
     def __init__(self):
-        pass
+        self.buzzer = Buzzer.Buzzer()
+        self.direction = Direction.Direction()
+        self.moteur = Moteur.Moteur()
+        self.tourelle = Tourelle.Tourelle()
 
-    def drive(self) :
-        pass
 
-    def reverse(self) :
-        pass
+    def drive(self, speed) :
+        self.moteur.drive(speed)
 
-    def turnTourelleXAxis(self, percentage) :
-        pass
+    def reverse(self, speed) :
+        self.moteur.reverse(speed)
+
+    def stop(self) :
+        self.moteur.stop()
+
+    def turnTourelleXAxis(self, angle) :
+        self.tourelle.turn_x_axis(angle)
     
-    def turnTourelleYAxis(self, percentage) :
-        pass
-
+    def turnTourelleYAxis(self, angle) :
+        self.tourelle.turn_y_axis(angle)
 
 if __name__ == "__main__" :
     try :
